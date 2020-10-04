656756c321320af3450f27ed62d1816f08f70bd8
ArtemKuksenko <kuksenko.artem@gmail.com>
2020-10-04 15:41:31 +0300

    clean

--- approximation.py
+++ approximation.py
@@ -22,7 +22,3 @@ def approximation_line(points):
     m = np.array([[m11, m12], [m12, n]])
     c = np.array([c1, c2])
     return np.linalg.solve(m, c)
-
-points = np.array([[1, 2, 3],
-                   [7, 7, 7]])
-print(approximation_line(points))
